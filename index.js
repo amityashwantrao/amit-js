38692f77bf9e1e8c12c011af3fb042ec8782f1b2
amityashwantrao99@gmail.com <amityashwantrao99@gmail.com>
2022-11-12 21:25:21 +0530

    js

---
+++ index.js
@@ -0,0 +1,5 @@
+document.write("hi welcome to external js")
+
+console.log("hi welcomr to javascript")
+
+alert('your age is='+age+'year');
---
+++ promt.js
@@ -0,0 +1,8 @@
+dob=prompt("enter your birth year");
+cyear=prompt("enter yout current year");
+age=dob-cyear;
+ 
+alert("your age"+age+"year");
+
+ 
+    
